4bff72d2a81d018b420fce784d27d20b499a1263
bhavadharani-sudhakaran-05 <ram05mahi03.gk@gmail.com>
2026-01-05 20:42:40 +0530

    Add CI Pipeline with github action

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT=process.env.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("Welcome to ShopEasy...!");
 })
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
+module.exports={app, server}
